e2c87292d368b5735c9b1ac9d776e94c179b65a4
Lam Nguyen <lamcnguyen89@gmail.com>
2022-01-12 22:22:34 -0500

    Added priority titling for clustered markers

--- AppleCarplay/Views/MapViewUI.swift
+++ AppleCarplay/Views/MapViewUI.swift
@@ -31,19 +31,35 @@ struct MapViewUI: UIViewRepresentable {
         
         func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
             
-            guard let placeAnnotation = annotation as? Place else {
-                return nil
+            switch annotation {
+            case let cluster as MKClusterAnnotation:
+                let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "cluster") as? MKMarkerAnnotationView ?? MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "cluster")
+                annotationView.markerTintColor = .brown
+                for clusterAnnotation in cluster.memberAnnotations {
+                    if let place = clusterAnnotation as? Place {
+                        if place.sponsored {
+                            cluster.title = place.name
+                            break
+                        }
+                    }
+                }
+                annotationView.titleVisibility = .visible
+                return annotationView
+            case let placeAnnotation as Place:
+                let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "InterestingPlace") as? MKMarkerAnnotationView ?? MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "Interesting Place")
+                annotationView.canShowCallout = true
+                annotationView.glyphText = "👀"
+                annotationView.clusteringIdentifier = "cluster"
+                annotationView.markerTintColor = UIColor(displayP3Red: 0.082, green: 0.518, blue: 0.263, alpha: 1.0)
+                annotationView.titleVisibility = .visible
+                annotationView.detailCalloutAccessoryView = UIImage(named: placeAnnotation.image).map(UIImageView.init)
+                
+                return annotationView
+                
+            default: return nil
             }
             
-            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "InterestingPlace") as? MKMarkerAnnotationView ?? MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "Interesting Place")
-            
-            annotationView.canShowCallout = true
-            annotationView.glyphText = "👀"
-            annotationView.markerTintColor = UIColor(displayP3Red: 0.082, green: 0.518, blue: 0.263, alpha: 1.0)
-            annotationView.titleVisibility = .visible
-            annotationView.detailCalloutAccessoryView = UIImage(named: placeAnnotation.image).map(UIImageView.init)
-            
-            return annotationView
+
         }
     }
     
