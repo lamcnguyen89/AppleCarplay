7fd43b00a6cc03eea095341d2f180f8231509bb3
Lam Nguyen <lamcnguyen89@gmail.com>
2022-01-15 20:04:52 -0500

    Playing around with adding some code for the Apple Carplay screen

--- AppleCarplay/CarPlaySceneDelegate.swift
+++ AppleCarplay/CarPlaySceneDelegate.swift
@@ -5,13 +5,62 @@ import CarPlay
 
 class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
     
-  func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
-                                  didConnect interfaceController: CPInterfaceController) {
+//  func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
+//                                  didConnect interfaceController: CPInterfaceController) {
+//    let screen = CPInformationTemplate(title: "Root", layout: .leading, items: [CPInformationItem(title: "Hello", detail: "CarPlay")], actions: [])
+//    interfaceController.setRootTemplate(screen, animated: true, completion: { _,_ in
+//        // Do nothing
+//    })
+//
+//  }
     
-    let screen = CPInformationTemplate(title: "Root", layout: .leading, items: [CPInformationItem(title: "Hello", detail: "CarPlay")], actions: [])
+    var interfaceController: CPInterfaceController?
     
-    interfaceController.setRootTemplate(screen, animated: true, completion: { _,_ in
-        // Do nothing
-    })
-  }
+    // Carplay connected
+    func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didConnect interfaceController: CPInterfaceController) {
+        self.interfaceController = interfaceController
+        
+        //creating the tab sections
+        let tabFavs = CPListItem(text: "Favorites", detailText: "subtitle for Favorites")
+        let tabRecent = CPListItem(text: "Most Recent", detailText: "subtitle for Most Recent")
+        let tabHistory = CPListItem(text: "History", detailText: "subtitle for History")
+        let tabSearch = CPListItem(text: "Search", detailText: "subtitle for Search")
+        
+        // adding the above tabs in section
+        let sectionItemsA = CPListSection(items: [tabFavs, tabRecent, tabHistory, tabSearch])
+        let sectionItemsB = CPListSection(items: [tabFavs, tabHistory])
+        
+        let listTemplate = CPListTemplate(title: "", sections: [sectionItemsA])
+        let listTemplateA = CPListTemplate(title: "", sections: [sectionItemsB])
+        let listTemplateB = CPListTemplate(title: "", sections: [sectionItemsA])
+        let listTemplateC = CPListTemplate(title: "", sections: [sectionItemsB])
+        
+        // Creating Tabs
+        let tabA: CPListTemplate = listTemplate
+        tabA.tabSystemItem = .favorites
+        tabA.showsTabBadge = false
+        
+        let tabB: CPListTemplate = listTemplateA
+        tabB.tabSystemItem = .mostRecent
+        tabB.showsTabBadge = true
+        
+        let tabC: CPListTemplate = listTemplateB
+        tabC.tabSystemItem = .history
+        tabC.showsTabBadge = false
+        
+        let tabD: CPListTemplate = listTemplateC
+        tabD.tabSystemItem = .search
+        tabD.showsTabBadge = false
+        
+        let tabBarTemplate = CPTabBarTemplate(templates: [tabA, tabB, tabC, tabD])
+        self.interfaceController?.setRootTemplate(tabBarTemplate, animated: true)
+        
+    }
+    
+    // CarPlay disconnected
+    private func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didDisconnect interfaceController: CPInterfaceController) {
+        print("Carplay disconnected. Goodbye!!")
+        self.interfaceController = nil
+    }
+    
 }
